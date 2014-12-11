f7418bd77c09f3263665881a80bc2e1461714c38
Budi <ondrej@budai.cz>
2014-12-11 16:57:16 +0100

    Once again...

--- Travis-deploy.sh
+++ Travis-deploy.sh
@@ -4,19 +4,16 @@ openssl aes-256-cbc -K $encrypted_3f2407c008a5_key -iv $encrypted_3f2407c008a5_i
 
 chmod 0600 .key
 
-sftp -oStrictHostKeyChecking=no -i .key  -b - ondrej.budai@bastl1.rss.tul.cz <<EOF
-mkdir /mnt/home/ondrej.budai/builds/${TRAVIS_BUILD_NUMBER}
-EOF
 
 case "${os}" in
   linux32)
 	sftp -oStrictHostKeyChecking=no -i .key  -b - ondrej.budai@bastl1.rss.tul.cz <<EOF
-put buildroot.i386/MakeWarNotScience /mnt/home/ondrej.budai/builds/${TRAVIS_BUILD_NUMBER}/MakeWarNotScience-linux32
+put buildroot.i386/MakeWarNotScience /mnt/home/ondrej.budai/builds/${TRAVIS_BUILD_NUMBER}-linux32
 EOF
     ;;
   linux64)
 	sftp -oStrictHostKeyChecking=no -i .key  -b - ondrej.budai@bastl1.rss.tul.cz <<EOF
-put MakeWarNotScience /mnt/home/ondrej.budai/builds/${TRAVIS_BUILD_NUMBER}/MakeWarNotScience-linux32
+put MakeWarNotScience /mnt/home/ondrej.budai/builds/${TRAVIS_BUILD_NUMBER}-linux64
 EOF
     ;;
 esac
--- Travis-script.sh
+++ Travis-script.sh
@@ -7,11 +7,9 @@ case "${os}" in
   linux32)
     sudo chroot ${PWD}/buildroot.i386 make -C ${PWD} clean
     sudo chroot ${PWD}/buildroot.i386 make -C ${PWD} -j2 CXX="g++-4.8" 
-    mv buildroot.i386/MakeWarNotScience MakeWarNotScience-linux32
     ;;
   linux64)
     make clean
     make -j2 CXX="g++-4.8"
-    mv MakeWarNotScience MakeWarNotScience-linux64
     ;;
 esac
