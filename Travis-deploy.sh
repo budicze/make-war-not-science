7a411e239555150b107e4b6705240e9eff4b3c03
Budi <ondrej@budai.cz>
2014-12-11 16:22:44 +0100

    This should in theory work...

--- Travis-deploy.sh
+++ Travis-deploy.sh
@@ -1,17 +1,19 @@
+#!/bin/sh
+
 openssl aes-256-cbc -K $encrypted_3f2407c008a5_key -iv $encrypted_3f2407c008a5_iv -in .key.enc -out .key -d
 
-TRAVIS_BUILD_NUMBER
+sftp -oStrictHostKeyChecking=no -i .key  -b - xbudai00@merlin.fit.vutbr.cz <<EOF
+mkdir /homes/eva/xb/xbudai00/builds/${TRAVIS_BUILD_NUMBER}
+EOF
 
 case "${os}" in
   linux32)
     sftp -oStrictHostKeyChecking=no -i .key  -b - xbudai00@merlin.fit.vutbr.cz <<EOF
-mkdir -p /homes/eva/xb/xbudai00/builds/${TRAVIS_BUILD_NUMBER}
 put buildroot.i386/MakeWarNotScience /homes/eva/xb/xbudai00/builds/${TRAVIS_BUILD_NUMBER}/MakeWarNotScience-linux32
 EOF
     ;;
   linux64)
 	sftp -oStrictHostKeyChecking=no -i .key  -b - xbudai00@merlin.fit.vutbr.cz <<EOF
-mkdir -p /homes/eva/xb/xbudai00/builds/${TRAVIS_BUILD_NUMBER}
 put MakeWarNotScience /homes/eva/xb/xbudai00/builds/${TRAVIS_BUILD_NUMBER}/MakeWarNotScience-linux32
 EOF
     ;;
