491cc44a69474bacb35bc7170897831f384e3750
Budi <ondrej@budai.cz>
2014-12-11 13:00:23 +0100

    Try to fix both builds

--- Travis-before-install.sh
+++ Travis-before-install.sh
@@ -8,14 +8,13 @@ case "$os" in
     # our dependencies to be able to compile a 32bit binary. Ubuntu...
     chroot="$PWD"/buildroot.i386
     mkdir -p "$chroot$PWD"
-    sudo apt-get install -y debootstrap
+    sudo apt-get install -y debootstrap python-software-properties
     sudo i386 debootstrap --arch=i386 precise "$chroot"
     sudo mount --rbind "$PWD" "$chroot$PWD"
     sudo i386 chroot "$chroot" add-apt-repository -y ppa:zoogie/sdl2-snapshots
     sudo i386 chroot "$chroot" add-apt-repository -y ppa:ubuntu-toolchain-r/test
     sudo i386 chroot "$chroot" apt-get update -qq
     sudo i386 chroot "$chroot" apt-get install -qq libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev g++-4.8
-    sudo i386 chroot "$chroot" export CXX="g++-4.8"
 
     ;;
   linux64)
@@ -23,6 +22,6 @@ case "$os" in
     sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
     sudo apt-get update -qq
     sudo apt-get install -qq libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev g++-4.8
-    export CXX="g++-4.8"
+    
     ;;
 esac
--- Travis-script.sh
+++ Travis-script.sh
@@ -5,10 +5,12 @@ set -x
 
 case "${os}" in
   linux32)
+    sudo chroot ${PWD}/buildroot.i386 export CXX="g++-4.8"
     sudo chroot ${PWD}/buildroot.i386 make clean
     sudo chroot ${PWD}/buildroot.i386 make -j2
     ;;
   linux64)
+    export CXX="g++-4.8"
     make clean
     make
     ;;
