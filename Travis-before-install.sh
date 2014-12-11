0669788d783d89d2a808ed322d8d9023816fa9d3
Budi <ondrej@budai.cz>
2014-12-11 12:45:40 +0100

    Try to fix linux32 build and speed up linux64 build

--- Travis-before-install.sh
+++ Travis-before-install.sh
@@ -11,9 +11,18 @@ case "$os" in
     sudo apt-get install -y debootstrap
     sudo i386 debootstrap --arch=i386 precise "$chroot"
     sudo mount --rbind "$PWD" "$chroot$PWD"
-    sudo i386 chroot "$chroot" apt-get install -qq libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev
+    sudo i386 chroot "$chroot" add-apt-repository -y ppa:zoogie/sdl2-snapshots
+    sudo i386 chroot "$chroot" add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    sudo i386 chroot "$chroot" apt-get update -qq
+    sudo i386 chroot "$chroot" apt-get install -qq libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev g++-4.8
+    sudo i386 chroot "$chroot" export CXX="g++-4.8"
+
     ;;
   linux64)
-    sudo apt-get install -qq libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev
+    sudo add-apt-repository -y ppa:zoogie/sdl2-snapshots
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    sudo apt-get update -qq
+    sudo apt-get install -qq libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev g++-4.8
+    export CXX="g++-4.8"
     ;;
 esac
