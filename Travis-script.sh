a66cfdc9771ff0870acbe15df8421fc57f69a451
Budi <ondrej@budai.cz>
2014-12-11 13:33:42 +0100

    Try to pass the variable by make, not by export command

--- Travis-script.sh
+++ Travis-script.sh
@@ -5,9 +5,8 @@ set -x
 
 case "${os}" in
   linux32)
-    sudo chroot ${PWD}/buildroot.i386 export CXX="g++-4.8"
     sudo chroot ${PWD}/buildroot.i386 make clean
-    sudo chroot ${PWD}/buildroot.i386 make -j2
+    sudo chroot ${PWD}/buildroot.i386 make CXX="g++-4.8" -j2
     ;;
   linux64)
     export CXX="g++-4.8"
