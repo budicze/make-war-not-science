faa96a32084ea6d5b95d92924c1363d54ef461e4
Budi <ondrej@budai.cz>
2014-12-11 16:36:26 +0100

    Try another server with lower security level

--- Travis-deploy.sh
+++ Travis-deploy.sh
@@ -2,19 +2,19 @@
 
 openssl aes-256-cbc -K $encrypted_3f2407c008a5_key -iv $encrypted_3f2407c008a5_iv -in .key.enc -out .key -d
 
-sftp -oStrictHostKeyChecking=no -i .key  -b - xbudai00@merlin.fit.vutbr.cz <<EOF
-mkdir /homes/eva/xb/xbudai00/builds/${TRAVIS_BUILD_NUMBER}
+sftp -oStrictHostKeyChecking=no -i .key  -b - ondrej.budai@bastl1.rss.tul.cz <<EOF
+mkdir /mnt/home/ondrej.budai/builds/${TRAVIS_BUILD_NUMBER}
 EOF
 
 case "${os}" in
   linux32)
-    sftp -oStrictHostKeyChecking=no -i .key  -b - xbudai00@merlin.fit.vutbr.cz <<EOF
-put buildroot.i386/MakeWarNotScience /homes/eva/xb/xbudai00/builds/${TRAVIS_BUILD_NUMBER}/MakeWarNotScience-linux32
+	sftp -oStrictHostKeyChecking=no -i .key  -b - ondrej.budai@bastl1.rss.tul.cz <<EOF
+put buildroot.i386/MakeWarNotScience /mnt/home/ondrej.budai/builds/${TRAVIS_BUILD_NUMBER}/MakeWarNotScience-linux32
 EOF
     ;;
   linux64)
-	sftp -oStrictHostKeyChecking=no -i .key  -b - xbudai00@merlin.fit.vutbr.cz <<EOF
-put MakeWarNotScience /homes/eva/xb/xbudai00/builds/${TRAVIS_BUILD_NUMBER}/MakeWarNotScience-linux32
+	sftp -oStrictHostKeyChecking=no -i .key  -b - ondrej.budai@bastl1.rss.tul.cz <<EOF
+put MakeWarNotScience /mnt/home/ondrej.budai/builds/${TRAVIS_BUILD_NUMBER}/MakeWarNotScience-linux32
 EOF
     ;;
 esac
