56a527da26e2925b58c3c006026ebeaa9f7734d6
Budi <ondrej@budai.cz>
2014-12-11 14:00:17 +0100

    Need to be in corrent directory

--- Travis-script.sh
+++ Travis-script.sh
@@ -5,8 +5,8 @@ set -x
 
 case "${os}" in
   linux32)
-    sudo chroot ${PWD}/buildroot.i386 make clean
-    sudo chroot ${PWD}/buildroot.i386 make CXX="g++-4.8" -j2
+    sudo chroot ${PWD}/buildroot.i386 make -C ${PWD} clean
+    sudo chroot ${PWD}/buildroot.i386 make -C ${PWD} -j2 CXX="g++-4.8" 
     ;;
   linux64)
     export CXX="g++-4.8"
