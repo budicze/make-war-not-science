f28d466a184ea879e082566bfe20063e79461d2e
Budi <ondrej@budai.cz>
2014-12-13 18:21:58 +0100

    Forgot to add some semicolons...

--- Travis-before-install.sh
+++ Travis-before-install.sh
@@ -23,6 +23,7 @@ case "$os" in
     sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
     sudo apt-get update -qq
     sudo apt-get install -qq libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev g++-4.8
+    ;;
   win32)
     sudo sed -i -e "s/precise/trusty/g" /etc/apt/sources.list
     sudo apt-get update -qq
